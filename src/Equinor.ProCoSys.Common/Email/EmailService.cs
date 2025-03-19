c9a4e0cad1f3a142e57f6ed991994097f393523e
Espen Hatleskog <eha@equinor.com>
2025-03-19 14:04:03 +0100

    Replace IOptions with TokenCredential in constructor (#49)
    * Replace IOptions with TokenCredential in constructor
    
    * new major version for breaking changes

--- src/Equinor.ProCoSys.Common/Email/EmailService.cs
+++ src/Equinor.ProCoSys.Common/Email/EmailService.cs
@@ -3,104 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Azure.Identity;
+using Azure.Core;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Graph;
 using Microsoft.Graph.Models;
 
-namespace Equinor.ProCoSys.Common.Email
+namespace Equinor.ProCoSys.Common.Email;
+
+public class EmailService(IOptionsMonitor<EmailOptions> emailOptions, TokenCredential emailTokenCredential, ILogger<EmailService> logger) : IEmailService
 {
-    public class EmailService : IEmailService
+    private readonly string _mailUserOid = emailOptions.CurrentValue.MailUserOid;
+    
+    public async Task SendEmailsAsync(List<string> emails, string subject, string body, CancellationToken cancellationToken = default)
     {
-        private readonly ILogger _logger;
-        private readonly string _mailCredentialTenantId;
-        private readonly string _mailCredentialClientId;
-        private readonly string _mailCredentialSecret;
-        private readonly string _mailUserOid;
+        EmailValidator.ValidateEmails(emails);
 
-        public EmailService(IOptionsMonitor<EmailOptions> emailOptions, IOptionsMonitor<GraphOptions> graphOptions, ILogger<EmailService> logger)
+        var graphServiceClient = new GraphServiceClient(emailTokenCredential);
+        var graphMessage = new Message
         {
-            _mailCredentialTenantId = graphOptions.CurrentValue.TenantId;
-            _mailCredentialClientId = graphOptions.CurrentValue.ClientId;
-            _mailCredentialSecret = graphOptions.CurrentValue.ClientSecret;
-            _mailUserOid = emailOptions.CurrentValue.MailUserOid;
-
-            _logger = logger;
-        }
-
-        public async Task SendEmailsAsync(List<string> emails, string subject, string body, CancellationToken cancellationToken = default)
+            Subject = subject,
+            Body = new ItemBody
+            {
+                ContentType = BodyType.Html,
+                Content = body
+            },
+            ToRecipients = emails.Select(x => new Recipient { EmailAddress = new EmailAddress { Address = x } }).ToList()
+        };
+        try
         {
-            EmailValidator.ValidateEmails(emails);
-
-            var credentials = new ClientSecretCredential(
-                _mailCredentialTenantId,
-                _mailCredentialClientId,
-                _mailCredentialSecret
-                );
-
-            var graphServiceClient = new GraphServiceClient(credentials);
-            var graphMessage = new Message
-            {
-                Subject = subject,
-                Body = new ItemBody
-                {
-                    ContentType = BodyType.Html,
-                    Content = body
-                },
-                ToRecipients = emails.Select(x => new Recipient { EmailAddress = new EmailAddress { Address = x } }).ToList()
-            };
-            try
-            {
-                await graphServiceClient.Users[_mailUserOid]
-                       .SendMail
-                       .PostAsync(new Microsoft.Graph.Users.Item.SendMail.SendMailPostRequestBody
-                       {
-                           Message = graphMessage,
-                           SaveToSentItems = false                           
-                       }, null, cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogWarning(ex, $"An email with subject {subject} could not be sent.");
-                throw new Exception($"It was not possible to send an email (subject: {subject})", ex);
-            }
-        }
-
-        public async Task SendMessageAsync(Message graphMessage,
-            CancellationToken cancellationToken = default)
-        {
-            if (graphMessage == null)
-            {
-                throw new ArgumentNullException(nameof(graphMessage));
-            }
-            if (graphMessage.ToRecipients == null || !graphMessage.ToRecipients.Any())
-            {
-                _logger.LogInformation("Tried to send mail without any recipients.");
-                throw new Exception("It was not possible to send an email since it does not contain any recipients.");
-            }
-            if (graphMessage.ToRecipients.Any(x => x.EmailAddress?.Address == null))
-            {
-                _logger.LogInformation("Tried to send mail to recipients without defining any address.");
-                throw new Exception("It was not possible to send an email since it does not contain any address for one or more recipients.");
-            }
-
-            EmailValidator.ValidateEmails(graphMessage.ToRecipients.Select(x => x.EmailAddress?.Address).ToList());
-
-            var credentials = new ClientSecretCredential(
-                _mailCredentialTenantId,
-                _mailCredentialClientId,
-                _mailCredentialSecret
-                );
-
-            var graphServiceClient = new GraphServiceClient(credentials);
-
             await graphServiceClient.Users[_mailUserOid]
-                .SendMail
-                .PostAsync(new Microsoft.Graph.Users.Item.SendMail.SendMailPostRequestBody() {
-                    Message = graphMessage, 
-                    SaveToSentItems = false 
-                }, null, cancellationToken);
+                   .SendMail
+                   .PostAsync(new Microsoft.Graph.Users.Item.SendMail.SendMailPostRequestBody
+                   {
+                       Message = graphMessage,
+                       SaveToSentItems = false                           
+                   }, null, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            logger.LogWarning(ex, $"An email with subject {subject} could not be sent.");
+            throw new Exception($"It was not possible to send an email (subject: {subject})", ex);
         }
     }
+
+    public async Task SendMessageAsync(Message graphMessage,
+        CancellationToken cancellationToken = default)
+    {
+        if (graphMessage == null)
+        {
+            throw new ArgumentNullException(nameof(graphMessage));
+        }
+        if (graphMessage.ToRecipients == null || !graphMessage.ToRecipients.Any())
+        {
+            logger.LogInformation("Tried to send mail without any recipients.");
+            throw new Exception("It was not possible to send an email since it does not contain any recipients.");
+        }
+        if (graphMessage.ToRecipients.Any(x => x.EmailAddress?.Address == null))
+        {
+            logger.LogInformation("Tried to send mail to recipients without defining any address.");
+            throw new Exception("It was not possible to send an email since it does not contain any address for one or more recipients.");
+        }
+
+        EmailValidator.ValidateEmails(graphMessage.ToRecipients.Select(x => x.EmailAddress?.Address).ToList());
+
+        var graphServiceClient = new GraphServiceClient(emailTokenCredential);
+
+        await graphServiceClient.Users[_mailUserOid]
+            .SendMail
+            .PostAsync(new Microsoft.Graph.Users.Item.SendMail.SendMailPostRequestBody() {
+                Message = graphMessage, 
+                SaveToSentItems = false 
+            }, null, cancellationToken);
+    }
 }
+
